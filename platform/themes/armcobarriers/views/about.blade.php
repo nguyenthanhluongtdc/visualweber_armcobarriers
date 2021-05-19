89ce29238f947c5ee6558e847a152eebcf9858ea
Tin <tin.pt@visualweber.com>
2021-05-19 19:16:26 +0700

    cat layout

--- platform/themes/armcobarriers/views/about.blade.php
+++ platform/themes/armcobarriers/views/about.blade.php
@@ -1,13 +1,130 @@
 {{-- <h3>{{ $page->name }} Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore sit, voluptate ullam quas provident debitis est odit. Sapiente explicabo quod, recusandae eum esse, asperiores ab aliquid tempora atque, aspernatur tenetur!</h3>
 {!! Theme::breadcrumb()->render() !!}
 {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!} --}}
-<div class="container">
+{{-- <div class="container">
     <div class="cont1">
     <a href="">Homepage</a></li>
     <span>|</span>
     <a href="">About Us</a></li>
     <div class="cont2">
-        <h1>Asset Protection Solution </h1>
+        <h1>Asset Protection Solutions </h1>
+        <h2>Roadside,Car Parks,Warehouse</h2>
     </div>
+    <div class="cont3">
+        <a href="">Company Profile</a></li>
+        <a href="">Produc Quality</a></li>
+    </div>
+    </div>
+</div>
+.<div class="container-fluid">
+    <div class="banner">
+        <img src="{{ Theme::asset()->url('images/about/img1.png') }}" alt="">
+    </div>
+    <div class="container">
+        <div class="body">
+            <div class= "row">
+                <div class ="col-sm-4">
+                    <span>With the experience of 25 year in guadrail and barrier systems,Armco
+                        Barriers Pty Ltd hasbecome the leader in the safety barrie makert.From humble
+                        beginnings,the company now has a clientele base that will only chose ARMCO for
+                        their safety barrier requirements. An allAustralian family owned business that
+                        employs Australians resoures,we provide service,support and aknowledge of the
+                        product that is second to none and will always see ourclients return.</span>
+                </div>
+             </div> 
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="title">
+        <h1>What We Do</h1>
+    </div>
+    <div class= "row">
+        <div class ="col-sm-4 ">
+            <div class="des1">
+                <h2>Innovation</h2>
+            </div>
+          
+        </div>
+        <div class ="col-sm-4">
+           <h2>Innovation</h2>
+        </div>
+        <div class ="col-sm-4">
+            <h2>Installation Service</h2>
+        </div>
+     </div>
+</div> --}}
+<div class="container">
+    <div class="wrap-top">
+        <div class="cont1">
+            <a href="">Homepage</a>
+            <span>|</span>
+            <a href="">About Us</a>
+        </div>
+        <div class="cont2">
+            <h1>Asset Protection Solutions </h1>
+            <h2>Roadside,Car Parks,Warehouse</h2>
+        </div>
+        <div class="cont3">
+            <a href="">Company Profile</a>
+            <a href="">Product Quality</a>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="wrap-cont">
+        <div class="baner">
+            <img src="{{ Theme::asset()->url('images/about/img1.png') }}" alt="">
+         <div class="container">
+            <div class="des">
+                <div class= "row">
+                    <div class ="col-sm-4">
+                        <span>With the experience of 25 year in guadrail and barrier systems,Armco
+                            Barriers Pty Ltd hasbecome the leader in the safety barrie makert.From humble
+                            beginnings,the company now has a clientele base that will only chose ARMCO for
+                            their safety barrier requirements. An allAustralian family owned business that
+                            employs Australians resoures,we provide service,support and aknowledge of the
+                            product that is second to none and will always see ourclients return.</span>
+                    </div>
+                 </div> 
+            </div>
+        </div> 
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="body">
+        <div class="title">
+            <h1>What We Do</h1>
+        </div>
+       <div class="des">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="design-advice">
+                    <div class="design-advice--title">
+                     <h3> Design Advice</h3>   
+                    </div>
+                </div>
+                <p>
+                    dsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaadjask
+                    ljdksajdkljsakldjasklbhwhqjkjkwqiajkjksdjksjksdjksda
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <p>
+                    dsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                    aaaaaaaaaaaadjaskljdksajdkljsakldjasklbhwhqjkjkwqiajkjksdjksjksdjksda
+
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <p>
+                    dsdaaaaaaaaaaaaaaaaaaaaaaaa
+                    aaaaaaaaaaaaaaaaadjaskljdksajdkljsakldjasklbhwhqjkjkwqiajkjksdjksjksdjksda
+
+                </p>
+            </div>
+          </div>
+       </div>
     </div>
 </div>
