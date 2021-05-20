b9eb546df99e47cf31bcc4dceb947ac73a8a1f22
Tin <tin.pt@visualweber.com>
2021-05-20 18:37:27 +0700

    html about

--- platform/themes/armcobarriers/views/about.blade.php
+++ platform/themes/armcobarriers/views/about.blade.php
@@ -1,59 +1,3 @@
-{{-- <h3>{{ $page->name }} Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore sit, voluptate ullam quas provident debitis est odit. Sapiente explicabo quod, recusandae eum esse, asperiores ab aliquid tempora atque, aspernatur tenetur!</h3>
-{!! Theme::breadcrumb()->render() !!}
-{!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!} --}}
-{{-- <div class="container">
-    <div class="cont1">
-    <a href="">Homepage</a></li>
-    <span>|</span>
-    <a href="">About Us</a></li>
-    <div class="cont2">
-        <h1>Asset Protection Solutions </h1>
-        <h2>Roadside,Car Parks,Warehouse</h2>
-    </div>
-    <div class="cont3">
-        <a href="">Company Profile</a></li>
-        <a href="">Produc Quality</a></li>
-    </div>
-    </div>
-</div>
-.<div class="container-fluid">
-    <div class="banner">
-        <img src="{{ Theme::asset()->url('images/about/img1.png') }}" alt="">
-    </div>
-    <div class="container">
-        <div class="body">
-            <div class= "row">
-                <div class ="col-sm-4">
-                    <span>With the experience of 25 year in guadrail and barrier systems,Armco
-                        Barriers Pty Ltd hasbecome the leader in the safety barrie makert.From humble
-                        beginnings,the company now has a clientele base that will only chose ARMCO for
-                        their safety barrier requirements. An allAustralian family owned business that
-                        employs Australians resoures,we provide service,support and aknowledge of the
-                        product that is second to none and will always see ourclients return.</span>
-                </div>
-             </div> 
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="title">
-        <h1>What We Do</h1>
-    </div>
-    <div class= "row">
-        <div class ="col-sm-4 ">
-            <div class="des1">
-                <h2>Innovation</h2>
-            </div>
-          
-        </div>
-        <div class ="col-sm-4">
-           <h2>Innovation</h2>
-        </div>
-        <div class ="col-sm-4">
-            <h2>Installation Service</h2>
-        </div>
-     </div>
-</div> --}}
 <div class="container">
     <div class="wrap-top">
         <div class="cont1">
@@ -73,58 +17,224 @@
 </div>
 <div class="container-fluid">
     <div class="wrap-cont">
-        <div class="baner">
+        <div class="banner">
             <img src="{{ Theme::asset()->url('images/about/img1.png') }}" alt="">
-         <div class="container">
-            <div class="des">
-                <div class= "row">
-                    <div class ="col-sm-4">
-                        <span>With the experience of 25 year in guadrail and barrier systems,Armco
-                            Barriers Pty Ltd hasbecome the leader in the safety barrie makert.From humble
-                            beginnings,the company now has a clientele base that will only chose ARMCO for
-                            their safety barrier requirements. An allAustralian family owned business that
-                            employs Australians resoures,we provide service,support and aknowledge of the
-                            product that is second to none and will always see ourclients return.</span>
-                    </div>
-                 </div> 
+            <div class="rec">
+                <img src="{{ Theme::asset()->url('images/about/rec1.png') }}" alt="">
             </div>
-        </div> 
+           <div class="content-about">
+            <span>With the experience of over 25 years in guardrail and barrier systems, Armco® 
+                Barriers Pty Ltd has become the leader in the safety barrier market.  From humble 
+                beginnings, the company now has a clientele base that will only choose ARMCO® for 
+                their safety barrier requirements.  An all Australian family owned business that
+                employs Australians and uses Australian resources, we provide service, support and a
+                knowledge of the product that is second to none and will always see our clients return.</span>
+           </div>
+        </div>
+        
+    </div>
+</div>
+<div class="container">
+    <div class="wrap-product">
+        <div class="whatwedo">
+            <h2> What We Do</h2>
+        </div>
+       <div class="wrap1">
+        <div class="row">
+            <div class="col-sm-4">
+               <div class="design">
+                   <h3>Design Advice</h3></br></br>
+                    <p>At ARMCO®  Barriers you deal with people specialising in the road 
+                        safety barrier industry with over 20 years experience.</p></br>
+                    <p>Many clients take advantage of our expertise prior to planning their
+                         project and achieve better solutions at a reduced cost.</p>
+               </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="innovation">
+                    <h3>Innovation</h3></br></br>
+                    <p>Expertise leads to Innovation.  Whilst our competitor’s product remains
+                    largely unchanged, ARMCO®  Barriers continue to innovate.
+                    Innovations such as symmetrical holes in the post for easier installation,
+                    or internal storage to prevent white rust are all reasons why ARMCO®
+                    Barriers is the preferred supplier for more and more customers.</p></br>
+                    <p>The development of new and relevant products such as the Stubby
+                    Nose™ terminal, Pipegard™, Cornergard™ , Bollards and Handrails
+                    reduce your Occupational Health & Safety concerns whilst providing
+                    practical and economical safety barrier solutions.</p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="installation">
+                    <h3>Installation Service</h3></br></br>
+                    <p>ARMCO®  Barriers is leading the way as a manufacturer that offers an 
+                    Installation service by its own staff with total product knowledge.
+                    Our teams are first aid, Traffic Management and OH&S trained to
+                    ensure the job is performed safely and to specifications.</p></br>
+                    <p>Selecting the manufacturer to install the product has the benefit of faster
+                    installation and the assurance that it has been correctly installed.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wrap2">
+        <div class="row">
+            <div class="col-sm-4">
+               <div class="competitive">
+                   <h3>Competitive Pricing</h3></br></br>
+                    <p>Providing better products, design advice, an installation service and
+                    superior Customer Service is our standard practice. </p></br>
+                    <p>ARMCO Barriers also offers competitive pricing each and every time.</p>
+               </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="warranty">
+                    <h3>Warranty Statenment</h3></br></br>
+                    <p>ARMCO®  Barriers warrants that its products are supplied free of
+                    defects in workmanship and materials.</p></br>
+                    <p>Provided it is correctly installed, is not damaged in a collision
+                    nor subject to misuse, all ARMCO®  products should give many years of
+                    maintenance-free, safety barrier protection. </p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="quality">
+                    <h3>Quality Assurance</h3></br></br>
+                    <p>Product excellence is assured by operating in accordance with Quality 
+                    Assurance System AS/NZS ISO 9000.2 . Our products meet the
+                    requirements of Australian Standards and/or local Road Authority Standards. </p></br>
+                    <p>All product is hot dipped Galvanized to conform to AS/NZS 4680. Our 
+                    installation services also meet the requirements of each State Road Authority. </p>
+                </div>
+            </div>
+        </div>
+       </div>
+       <div class="wrap3">
+        <div class="row">
+            <div class="col-sm-4">
+               <div class="availability">
+                   <h3>Availability</h3></br></br>
+                    <p>Armco®  Barriers is a manufacturer of all of it's products, therefore we
+                    carry stock and availability is not an issue. </p></br>
+                    <p>Custom products or 'specials' can also be available in a very short time 
+                    frame, due to accessibility to production, material and professional personnel
+                    to ensure that a product is available as soon as possible.</p>
+               </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="australian">
+                    <h3>Australian Owned</h3></br></br>
+                    <p>ARMCO®  Barriers is a wholly Australian owned company supplying
+                    and operating throughout the nation and will satisfy your safety barrier requirements Australia wide.  </p>
+                </div>
+            </div>
+        </div>
+       </div>
+       <div class="wrap4">
+           <div class="icon">
+            <img src="{{ Theme::asset()->url('images/about/iconarrow.png') }}" alt="">
+           </div>
+           <div class="view">
+               <p>VIEW OUR PRODUCT</p>
+           </div>
+       </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="hero">
+        <img src="{{ Theme::asset()->url('images/about/hero.png') }}" alt="">
+    </div>
+</div>
+<div class="container">
+    <div class="wrap-bot">
+        <div class="bot1">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="product-quality">
+                        <h2>Product Quality</h2>
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <div class="slogan">
+                        <p>Armco®  Barriers Pty Ltd continually make innovations that improve the life, installation time and performance of our product, all the while 
+                        maintaining regulatory standards from all government authorities. We constantly research and test our products with structural engineers
+                        and authorized testing facilities to ensure that the customer will be purchasing the best product available.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bot2">
+            <div class="row">
+                <div class="col-sm-4">
+                </div>
+                <div class="col-sm-4">
+                    <div class="product1">
+                        <img src="{{ Theme::asset()->url('images/about/product1.png') }}" alt="">
+                        <h3>Galvanizing</h3>
+                        <p>All materials meet the requirements of relevant State Authority 
+                        standards for crash barriers and are hot dip galvanized to AS/NZS 4680
+                        after manufacture.</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="product2">
+                        <img src="{{ Theme::asset()->url('images/about/product2.png') }}" alt="">
+                        <h3>Powder Coating</h3>
+                        <p>Armco®  Barriers can offer their entire product range powder coated. 
+                        Safety yellow being the most popular colour requested.</p></br>
+                        <p>A full range of colours is available to suit corporate or environmental
+                         requirements.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 <div class="container">
-    <div class="body">
-        <div class="title">
-            <h1>What We Do</h1>
-        </div>
-       <div class="des">
+    <div class="count">
         <div class="row">
             <div class="col-sm-4">
-                <div class="design-advice">
-                    <div class="design-advice--title">
-                     <h3> Design Advice</h3>   
-                    </div>
+                <div class="count-customers">
+                    <h2>15,451</h2>
+                    <p>Customers</p>
                 </div>
-                <p>
-                    dsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaadjask
-                    ljdksajdkljsakldjasklbhwhqjkjkwqiajkjksdjksjksdjksda
-                </p>
             </div>
             <div class="col-sm-4">
-                <p>
-                    dsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                    aaaaaaaaaaaadjaskljdksajdkljsakldjasklbhwhqjkjkwqiajkjksdjksjksdjksda
-
-                </p>
+                <div class="count-completed">
+                    <h2>125,451</h2>
+                    <p>Completed projects</p>
+                </div>
             </div>
             <div class="col-sm-4">
-                <p>
-                    dsdaaaaaaaaaaaaaaaaaaaaaaaa
-                    aaaaaaaaaaaaaaaaadjaskljdksajdkljsakldjasklbhwhqjkjkwqiajkjksdjksjksdjksda
-
-                </p>
+                <div class="count-kilomet">
+                    <h2>1,204</h2>
+                    <p>Kilometres installed</p>
+                </div>
             </div>
-          </div>
-       </div>
+        </div>
+    </div>
+    <div class="experience">
+        <div class="exp">
+            <p>At ARMCO® Barriers you deal with people specialising in the safety barrier industry with over 35 years experience. </p>
+        </div>
     </div>
 </div>
+<div class="container">
+    <div class="wrap-sign"> 
+       <div class="gr">
+            <div class="sign-up">
+                <p>Sign up to receive the</p>
+                <h3>latest insights</h3>
+            </div>
+            <div class="info-me">
+                <input type="text" placeholder="Full Name">
+                <input type="text" placeholder="Your Email">
+                <input type="text" placeholder="Your Company">
+                <button type ="submit">Sign Up</button>
+            </div>
+           
+
+        </div>
+    </div>
+</div>
+    
