c05e148bcf7fee6b6cd33f9ff5e8868d6c455743
Tin <tin.pt@visualweber.com>
2021-05-25 10:08:18 +0700

    no message

--- platform/themes/armcobarriers/views/service-detail.blade.php
+++ platform/themes/armcobarriers/views/service-detail.blade.php
@@ -1,3 +1,13 @@
-<div class="conten">
-    <h1>hello</h1>
-</div>
+<section>
+    <div class="container-customize">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Homepage</a></li>
+          <li class="breadcrumb-item"><a href="#">Services</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Installation</li>
+        </ol>
+      </nav> 
+  </section>
+  <section>
+      
+  </section>
