dd506b7eadffb3413298eaf6f627946a033c6565
Tin <phanthanhtin1703@gmail.com>
2021-06-02 13:38:49 +0700

    no message

--- platform/themes/armcobarriers/views/about.blade.php
+++ platform/themes/armcobarriers/views/about.blade.php
@@ -220,18 +220,28 @@
 </div>
 <div class="container-customize">
     <div class="wrap-sign">
-            <div class="info-me">
-                <div class="sign-up">
+            <div class="info-me flex-wrap py-5">
+                <div class="sign-up col-lg-3 col-12">
                     <p>Sign up to receive the</p>
                     <h3>latest insights</h3>
                 </div>
-                <input type="text" placeholder="Full Name">
-                <input type="text" placeholder="Full Name">
-                <input type="text" placeholder="Full Name">
-                <button type ="submit">Sign Up</button>
+                <form action="#" method="POST" class="col-lg-9 col-12 px-0">
+                    <div class="form-flex-lg">
+                        <p class="col-lg-28 col-lg-6 col-12 my-2">
+                            <input class="input__field" type="text" placeholder="Full Name">
+                        </p>
+                        <p class="col-lg-28 col-lg-6 col-12 my-2">
+                            <input class="input__field" type="text" placeholder="Your Email">
+                        </p>
+                        <p class="col-lg-28 col-lg-6 col-12 my-2">
+                            <input class="input__field" type="text" placeholder="Your Company">
+                        </p>
+                        <p class="col-lg-16 col-lg-6 col-12 my-2"> 
+                            <button class="button__field" type ="submit">Sign Up</button>
+                        </p>
+                    </div>
+                </form> 
+            </div>
         </div>
     </div>
 </div>
-
-    
-
