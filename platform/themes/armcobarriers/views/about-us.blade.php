75d0db9982f01d6ab8d2e074952793a50c8a2a39
Tin <phanthanhtin1703@gmail.com>
2021-06-08 16:44:11 +0700

    no message

--- platform/themes/armcobarriers/views/about-us.blade.php
+++ platform/themes/armcobarriers/views/about-us.blade.php
@@ -32,7 +32,7 @@
                         <div class="container-customize h-100">
                             <div class="content-about d-flex align-items-center h-100">
                                 <div class="col-lg-6">
-                                    @php echo get_sub_field($tab, 'tabs_decription') @endphp
+                                   <p> @php echo get_sub_field($tab, 'tabs_description') @endphp</p>
                                 </div>
                             </div>
                         </div>
