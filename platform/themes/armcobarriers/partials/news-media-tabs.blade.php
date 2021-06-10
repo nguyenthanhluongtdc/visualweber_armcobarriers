d308af23dc1cca16fbd90212cd8aa175613f9e7c
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-10 13:46:46 +0700

    fix

--- platform/themes/armcobarriers/partials/news-media-tabs.blade.php
+++ platform/themes/armcobarriers/partials/news-media-tabs.blade.php
@@ -17,7 +17,7 @@
                                                 <div class="row">
                                                     <div class="col-lg-5 col-md-5 col-sm-6 col-6 pr-0 mb-3 mb-sm-0">
                                                         <a href="'.$new->url.'">
-                                                            <img class="mw-100" src="'.RvMedia::getImageUrl($new->image).' }}" alt="">
+                                                            <img class="mw-100" src="'.RvMedia::getImageUrl($new->image).'" alt="">
                                                         </a>
                                                     </div>
                                                     <div class="col-lg-7 col-md-7 col-sm-6 col-6">
