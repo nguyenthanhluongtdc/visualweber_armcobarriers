99faf38735cb20a760d81f2da79c0df9b6e961d8
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-14 23:49:49 +0700

    fix

--- platform/themes/armcobarriers/views/news-media.blade.php
+++ platform/themes/armcobarriers/views/news-media.blade.php
@@ -3,36 +3,13 @@
 <!--title-->
 <div class="pages-news-media">
     <div class="container-customize">
-        <h1> Lastest <br> New & Events </h1>
-        <div class="box-media pt-5 pb-lg-5">
-            <div class="content">
-                <div class="row">
-                    @foreach(get_posts_by_category(17,3) as $event)
-                        <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-5 mb-md-0">
-                            <div class="item">
-                                <a href="{{$event->url}}">
-                                    <img src="{{ RvMedia::getImageUrl($event->image) }}" alt="">
-                                    <h3>
-                                        {{$event->name}}
-                                    </h3>
-                                    <code>
-                                        {{$event->description}}
-                                    </code>
-                                </a>
-
-                                <div class="options">
-                                    <div class="date"> {{$event->created_at->format('j F Y') }} </div>
-                                    <div class="box">
-                                        <a class="share"> Share </a>
-                                        <a class="type"> {!! $event->categories->first()->name !!} </a>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
+        {!!
+            Menu::renderMenuLocation('category-page-news-media', [
+                'options' => [],
+                'theme' => true,
+                'view' => 'category-latest-new',
+            ])
+        !!}
 
         {!!
             Menu::renderMenuLocation('tabs-menu', [
