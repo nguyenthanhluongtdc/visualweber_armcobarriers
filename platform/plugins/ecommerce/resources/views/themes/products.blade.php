3345c381c9933d461bb199b8acd5f2c018c0e0bb
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-16 10:56:43 +0700

    fix

--- platform/plugins/ecommerce/resources/views/themes/products.blade.php
+++ platform/plugins/ecommerce/resources/views/themes/products.blade.php
@@ -94,7 +94,7 @@
                     <div class="product-filter justify-content-lg-end">
                       <div class="product_filter--search">
                         <form action="{{route('public.products')}}" method="GET" class="form__search">
-                          <input type="text" class="header__search-input form-control form-control-sm submit-form-on-change first_null not_chosen"  placeholder="{{$query!=''?$query:'Search Product'}}" name="q"/>
+                          <input type="text" class="header__search-input form-control form-control-sm submit-form-on-change first_null not_chosen"  placeholder="{{$query==''?'Search Product':''}}" value="{{$query!=''?$query:''}}" name="q"/>
                           <button class="btn btn-secondary" type="submit">
                               <i class="fas fa-search"></i>
                           </button>
