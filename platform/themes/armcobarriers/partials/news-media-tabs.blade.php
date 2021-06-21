87ad4d4e901d9be9bff76a4cbc219ff62b5df973
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-21 20:07:23 +0700

    fox

--- platform/themes/armcobarriers/partials/news-media-tabs.blade.php
+++ platform/themes/armcobarriers/partials/news-media-tabs.blade.php
@@ -77,25 +77,21 @@
         })
 
         //click tag a paginate
-        if(scroll) {
-            $(document).on('click', '.pagination a', function(event){
-                event.preventDefault(); 
+        $(document).on('click', '.pagination a', function(event){
+            event.preventDefault(); 
+            if(scroll) {
                 let path = $(this).attr('href');
                 path = "/"+path.substr(path.indexOf('/',8) + 1)
                 fetch_ajax(path)
-            });
-        }else {
-            $(document).on('click', '.pagination a', function(event){
-                event.preventDefault(); 
+            }else {
                 let num = $(this).attr('href').split('?page=')[1];
-                
                 if(paths.length > 0){
                     let path = paths[0]+`?page=${num}`;
                     fetch_ajax(path)
                 }
-                
-            });
-        }
+            }
+        });
+        
 
         //method fetch ajax
         function fetch_ajax(path) {
