29e44f783816f1947e6ce035bb9b5339bc9edb64
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-21 16:52:20 +0700

    add action share

--- platform/themes/armcobarriers/partials/category-latest-new.blade.php
+++ platform/themes/armcobarriers/partials/category-latest-new.blade.php
@@ -1,7 +1,10 @@
 @php $post_by_category = get_posts_by_category_unpaginate($menu_nodes[0]->reference_id,3); @endphp
 
+<div class="animate slide-in-down notification-button">
+    <i class="fa fa-files-o"></i> Link Copied to Clipboard
+</div>
 <h1> Lastest <br> New & {{isset($menu_nodes[0])?$menu_nodes[0]->title:''}} </h1>
-<div class="box-media pt-5 pb-lg-5">
+<div class="box-media pt-5 pb-5">
     <div class="content">
         <div class="row">
             @if(!empty($post_by_category))
@@ -15,16 +18,18 @@
                                 <h3>
                                     {{$event->name}}
                                 </h3>
-                                <code>
+                                <div class="descriptions">
                                     {{$event->description}}
-                                </code>
+                                </div>
                             </a>
                             <div class="options">
                                 <div class="date"> {{$event->created_at->format('j F Y') }} </div>
                                 <div class="box">
-                                    <a class="share"> Share </a>
+                                    <span class="share"> Share </span>
+                                    <input class="share-url" value="{{$event->url}}"/>  
+                                    <button class="btn-copy"> <i class="fal fa-copy"></i> </button>
                                     <a class="type"> {!! $event->categories->first()->name !!} </a>
-                            </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -33,3 +38,38 @@
         </div>
     </div>
 </div>
+
+<script>
+    let $btnShare = $('.share');
+    let $notificationButton = $('.notification-button');
+    let $btnCopy = $('.btn-copy');
+    let $input_url = $('.share-url');
+    $share = false;
+
+    $btnShare.on('click',function(event){
+        event.preventDefault(); 
+        $(this).next().toggleClass('active');
+        $(this).next().next().toggleClass('active');
+        if(!$share){
+            $(this).text("Unshare");
+            $share = true;
+        }else {
+            $share = false;
+            $(this).text('Share');
+        }
+    });
+
+    function fadeOutNotification(){
+        setTimeout(function(){
+            $notificationButton.removeClass('active');
+        }, 2000);
+    }
+    
+    $btnCopy.on('click', function() {
+        $notificationButton.toggleClass('active');
+        var text = $(this).prev().select();
+        document.execCommand('copy');
+    });
+
+    $notificationButton.on('transitionend', fadeOutNotification);
+</script>
