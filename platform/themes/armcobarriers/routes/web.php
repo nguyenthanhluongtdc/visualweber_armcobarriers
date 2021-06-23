37712a7c74e00ffb7258b57d5c0562709b17674a
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-23 16:52:06 +0700

    fix

--- platform/themes/armcobarriers/routes/web.php
+++ platform/themes/armcobarriers/routes/web.php
@@ -5,8 +5,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Platform\Service\Models\Service;
-use Platform\Service\Models\Solution;
-use Platform\Page\Models\Page;
 
 Route::group(['namespace' => 'Theme\Armcobarriers\Http\Controllers', 'middleware' => ['web', 'core']], function () {
     Route::group(apply_filters(BASE_FILTER_GROUP_PUBLIC_ROUTE, []), function () {
@@ -15,11 +13,10 @@ Route::group(['namespace' => 'Theme\Armcobarriers\Http\Controllers', 'middleware
             'as' => 'service-detail',
             'uses' => 'ArmcobarriersController@getServices',
         ]);
-        
+
         Route::get('/news-media/ajax','ArmcobarriersController@getPostAjax')->name('public.post.ajax');
     });
 });
-
 
 // Theme::routes();
 
@@ -31,14 +28,9 @@ Route::group(['namespace' => 'Theme\Armcobarriers\Http\Controllers', 'middleware
 
         Route::get('sitemap.xml', 'ArmcobarriersController@getSiteMap')
             ->name('public.sitemap');
-        Route::get('{slug?}' . config('core.base.general.public_single_ending_url')
 
-        , 'ArmcobarriersController@getView')
+        Route::get('{slug?}' . config('core.base.general.public_single_ending_url'), 'ArmcobarriersController@getView')
             ->name('public.single');
-            Route::get(\SlugHelper::getPrefix(Solution::class) . '/{slug}', [
-                'as' => 'solutions-details',
-                'uses' => 'ArmcobarriersController@getSolutions',
-            ]);
             
     });
 });
