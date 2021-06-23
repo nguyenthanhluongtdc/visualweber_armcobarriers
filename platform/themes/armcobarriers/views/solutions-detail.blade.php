14556656e89e2d724e1cfcbaa90d0e9ec3b5f8df
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-23 21:28:21 +0700

    fix

--- platform/themes/armcobarriers/views/solutions-detail.blade.php
+++ platform/themes/armcobarriers/views/solutions-detail.blade.php
@@ -44,7 +44,7 @@
             </div>
         </div>
         <div class="right">
-            <img src="{{ RvMedia::getImageUrl(get_field($solution, 'banner_description_solution')) }}" alt="">
+            <img src="{{ RvMedia::getImageUrl(get_field($solution, 'big_picture_solution')) }}" alt="">
         </div>
     </div>
 @else
