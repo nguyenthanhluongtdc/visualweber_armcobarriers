44bd91c02f3a9b6d424f4336a85b2e5b956bee61
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-23 13:12:03 +0700

    fix

--- platform/themes/armcobarriers/views/modules/form-signup.blade.php
+++ platform/themes/armcobarriers/views/modules/form-signup.blade.php
@@ -1,27 +1,10 @@
 
-@php $message = ""; @endphp
-
-@if(session()->has('success_msg') || session()->has('error_msg') || isset($errors))
-    @if (session()->has('success_msg'))
-       @php $message = section('success_msg'); @endphp
-    @endif
-    @if (session()->has('error_msg'))
-        @php $message = section('error_msg'); @endphp
-    @endif
-    @if (isset($errors) && count($errors))
-        @foreach ($errors->all() as $error)
-             @php $message = $error; @endphp
-        @endforeach
-    @endif
-@endif
-
 <div class="wrap-sign">
     <div class="info-me flex-wrap py-5">
         <div class="sign-up col-lg-3 col-12">
             <p>Sign up to receive the</p>
             <h3>latest insights</h3>
         </div>
-      
         <form action="{{ route('public.newsletter.subscribe') }}" method="POST" class="col-lg-9 col-12 px-0">
          @csrf
             <div class="form-flex-lg">
@@ -42,47 +25,23 @@
     </div>
 </div>
 
-@if($message)
+@if(session()->has('success_msg') || session()->has('error_msg') || isset($errors) && count($errors))
     <div class="animate slide-in-down notification-button active">
-        <i class="fa fa-files-o"></i> 
-        {{$message}}
+    <i class="fa fa-files-o"></i> 
+        @if (session()->has('success_msg'))
+            <span> {{ session('success_msg') }} </span>
+        @endif
+        @if (session()->has('error_msg'))
+            <span>{{ session('error_msg') }}</span>
+        @endif
+        @if (isset($errors) && count($errors))
+            @foreach ($errors->all() as $error)
+                <span>{{ $error }}</span> <br>
+            @endforeach
+        @endif
     </div>
 @endif
 
-<style>
-/*fade*/
-.animate {
-    opacity: 0;
-    transition: all 1s;
-    -webkit-animation-duration: 1s;
-    animation-duration: 1s;
-    -webkit-animation-fill-mode: both;
-    animation-fill-mode: both;
-  }
-  
-  .animate.active {
-    opacity: 1;
-    transform: translateX(-50%) translateY(-50%);
-  }
-  
-  .slide-in-down {
-    transform: translateY(-100%) translateX(-50%);
-  }
-  
-  /*notification*/
-  .notification-button {
-    position: fixed;
-    top: 40px;
-    left: 50%;
-    padding: 10px 20px;
-    background: #FAD906;
-    color: #000;
-    border-radius: 5px;
-    font-size: 14px;
-    font-weight: 800;
-    box-shadow: 0 0 14px rgba(0, 0, 0, 0.05);
-}
-</style>
 <script>
     let $notificationButton = $('.notification-button');
 
