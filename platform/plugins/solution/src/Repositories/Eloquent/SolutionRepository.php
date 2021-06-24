6fa4f958772f2f64034507859096003e916d94c1
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-06-24 20:38:37 +0700

    no message

--- platform/plugins/solution/src/Repositories/Eloquent/SolutionRepository.php
+++ platform/plugins/solution/src/Repositories/Eloquent/SolutionRepository.php
@@ -17,23 +17,26 @@ class SolutionRepository extends RepositoriesAbstract implements SolutionInterfa
         return $this->applyBeforeExecuteQuery($data)->get();
     }
     public function getFeatured(int $limit = 3){
+        
         $data = $this->model
         ->where([
             'app_solutions.status'      => BaseStatusEnum::PUBLISHED,
             'app_solutions.is_featured' => 1,
         ])
         ->limit($limit)
-        ->orderBy('app_solutions.created_at', 'desc');
-
+        ->orderBy('app_solutions.created_at', 'desc')->get();
+            return $data;
     return $this->applyBeforeExecuteQuery($data)->get();
     }
     public function getLatest(){
+       
         $data = $this->model
         ->where([
             'app_solutions.status'      => BaseStatusEnum::PUBLISHED,
             'app_solutions.is_featured' => 1,
         ])
         ->orderBy('app_solutions.created_at', 'ASC');
+       
 
         return $this->applyBeforeExecuteQuery($data)->take(4)->get();
         }
