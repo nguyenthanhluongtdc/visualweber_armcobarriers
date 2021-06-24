c7f19bb4c9916a6ea50138906f53c8dc26031657
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-24 14:31:24 +0700

    fix

--- platform/themes/armcobarriers/views/solutions-detail.blade.php
+++ platform/themes/armcobarriers/views/solutions-detail.blade.php
@@ -35,7 +35,7 @@
 </div>
 @if(!empty(get_field($solution, 'banner_description_solution')))
     <div class="service-detail-banner">
-        <div class="left col-lg-6 col-md-8">
+        <div class="left ">
             <h4 class="left-title">
                 {{ get_field($solution, 'banner_title_solution') }}
             </h4>
