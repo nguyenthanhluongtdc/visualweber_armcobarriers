36b042cb47edeac19780b74b19a699f25524c3db
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-06-29 12:25:10 +0700

    fix

--- platform/plugins/ecommerce/resources/views/themes/child.blade.php
+++ platform/plugins/ecommerce/resources/views/themes/child.blade.php
@@ -1,5 +1,5 @@
 @foreach($categories as $category)
 <li>
-    <a class="section-armco__header__cate" href="{{$category->url}}"> {{$category->name}} </a>
+    <a href="{{$category->url}}"> {{$category->name}} </a>
 </li>
 @endforeach
