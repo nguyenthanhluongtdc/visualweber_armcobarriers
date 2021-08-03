c609ae026951539e47ca1aa0a30865ce8d2dd5cd
Toan LE <toan@visualweber.com>
2021-08-03 13:02:15 +0700

    Update platform/plugins/solution/database/migrations/2021_08_03_155844_add_ordering_to_app_solutions_table.php

--- platform/plugins/solution/database/migrations/2021_08_03_155844_add_ordering_to_app_solutions_table.php
+++ platform/plugins/solution/database/migrations/2021_08_03_155844_add_ordering_to_app_solutions_table.php
@@ -14,7 +14,7 @@ class AddOrderingToAppSolutionsTable extends Migration
     public function up()
     {
         Schema::table('app_solutions', function (Blueprint $table) {
-            //
+            $table->tinyInteger('ordering')->nullable();
         });
     }
 
